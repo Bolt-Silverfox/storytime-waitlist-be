cb46ea344880a1c81262d86694da23e02aefd700
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2025-11-23 11:09:45 +0100

    fix: save contact us data to database

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
 import { EmailModule } from '../email/email.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, PrismaModule],
   controllers: [ContactController],
   providers: [ContactService],
 })
--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateContactDto } from '../contact/dto/create-contact.dto';
 import { EmailService } from '../email/email.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ContactService {
-  constructor(private readonly emailService: EmailService) {}
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly prisma: PrismaService,
+    ) {}
 
   async handleContact(payload: CreateContactDto) {
     const { name, email, message } = payload;
+
+    await this.prisma.contactMessage.create({
+      data: { name, email, message },
+    });
 
     await this.emailService.sendContactEmail(name, email, message);
 
